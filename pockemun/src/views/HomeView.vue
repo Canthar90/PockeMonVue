ef817eb2acaa540222971e17ee940ef85e381108
Canthar90 <d.cieslak90@gmail.com>
2023-11-20 22:34:14 +0100

    Added first requests

--- pockemun/src/views/HomeView.vue
+++ pockemun/src/views/HomeView.vue
@@ -1,9 +1,26 @@
 <template>
   <main>
-    <div class="p-8 bg-slate-500 rounded-xl"></div>
+    <div v-for="pokemon in allPokemons" :key="pokemon.name" class="p-8 bg-slate-500 rounded-xl">
+      <h1 class="text-xl text-orange-950">{{ pokemon.name }}</h1>
+    </div>
   </main>
 </template>
 
 <script setup lang="ts">
-// Any differences anything testing
+import axios from 'axios'
+import { onBeforeMount, ref } from 'vue'
+
+interface PokemonData {
+  name: string
+  url: string
+}
+
+const allPokemons = ref<PokemonData[]>([])
+
+onBeforeMount(async () => {
+  const url = 'https://pokeapi.co/api/v2/pokemon?limit=2000'
+
+  const response = await axios.get(url)
+  allPokemons.value = response.data.results
+})
 </script>
