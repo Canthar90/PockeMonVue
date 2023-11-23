a48271a5bb03e4d2e054943fff22b066bfa0fd47
Canthar90 <d.cieslak90@gmail.com>
2023-11-23 16:35:39 +0100

    Fixed problem answer was in h-screen

--- pockemun/src/views/HomeView.vue
+++ pockemun/src/views/HomeView.vue
@@ -1,6 +1,6 @@
 <template>
   <div
-    class="w-full h-full bg-center bg-cover bg-no-repeat"
+    class="w-full h-screen bg-center bg-cover bg-no-repeat flex items-center justify-center"
     style="
       background-image: url('https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/0906b988-7e73-42cc-9161-23f7d070a0e0/derph79-29509d72-4009-4bf6-863a-8877f9935b27.jpg/v1/fill/w_1154,h_692,q_70,strp/pokemon_generation_3_megas_by_mcgmark_derph79-pre.jpg?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7ImhlaWdodCI6Ijw9OTAwIiwicGF0aCI6IlwvZlwvMDkwNmI5ODgtN2U3My00MmNjLTkxNjEtMjNmN2QwNzBhMGUwXC9kZXJwaDc5LTI5NTA5ZDcyLTQwMDktNGJmNi04NjNhLTg4NzdmOTkzNWIyNy5qcGciLCJ3aWR0aCI6Ijw9MTUwMCJ9XV0sImF1ZCI6WyJ1cm46c2VydmljZTppbWFnZS5vcGVyYXRpb25zIl19.rCdwYcjK24jB0jGKTm-9sQBQtk4pbpgVox6b4kUiORg');
     "
