08387659ce193b0b0a690376dec39531f653296a
Canthar90 <d.cieslak90@gmail.com>
2023-11-27 22:59:52 +0100

    Added router and some basic structure for third viev

--- pockemun/src/router/index.ts
+++ pockemun/src/router/index.ts
@@ -1,6 +1,7 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
 import PokemonDetails from '../views/PokemonDetails.vue'
+import PokemonSkill from '../views/PokemonSkill.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -14,6 +15,12 @@ const router = createRouter({
       path: '/pokemon/details/:pokemonName',
       name: 'pokemonDetails',
       component: PokemonDetails,
+      props: true
+    },
+    {
+      path: '/pokemon/skill/:skillName',
+      name: 'pokemonSkill',
+      component: PokemonSkill,
       props: true
     }
   ]
