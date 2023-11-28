43def4c7b5fdfa90b33db67be83b4d1a9f572ca6
Canthar90 <d.cieslak90@gmail.com>
2023-11-28 22:23:59 +0100

    Added typing for Choosen pokemon details and the displayed art was also changed

--- pockemun/src/views/PokemonDetails.vue
+++ pockemun/src/views/PokemonDetails.vue
@@ -1,12 +1,15 @@
 <template>
   <div class="flex items-center justify-center">
     <div class="grid grid-cols-10 mt-10 mb-10">
-      <div class="p-10 col-span-8 col-start-2 bg-red-300 rounded-3xl">
+      <div
+        v-if="fetched && choosenPokemon"
+        class="p-10 col-span-8 col-start-2 bg-red-300 rounded-3xl"
+      >
         <div class="flex justify-center justify-items-center">
           <img
-            :src="choosenPokemon.sprites.other.home.front_default"
+            :src="choosenPokemon.sprites.other['official-artwork'].front_default"
             alt="Pokemon image"
-            class="bg-slate-200 rounded-full w-2/5"
+            class="bg-slate-200 rounded-full"
           />
         </div>
         <div class="text-6xl flex justify-center pt-2 text-slate-950">
@@ -19,10 +22,18 @@
             :key="move.move.name"
             class="bg-red-500 rounded-2xl flex justify-center text-slate-950"
             role="button"
+            @click="goToPokemonSkill(move.move.name)"
           >
             {{ move.move.name }}
           </div>
         </div>
+      </div>
+
+      <div
+        v-else
+        class="p-10 col-span-8 col-start-2 bg-red-300 rounded-3xl text-slate-950 text-5xl"
+      >
+        Loading...
       </div>
     </div>
   </div>
@@ -39,7 +50,29 @@ const prop = defineProps({
   }
 })
 
-const choosenPokemon = ref()
+type Move = {
+  move: {
+    name: string
+    url: string
+  }
+  version_group_details: Record<string, any>
+}
+
+type PokemonDetails = {
+  name: string
+  moves: Array<Move>
+  sprites: {
+    other: {
+      'official-artwork': {
+        front_default: string
+      }
+    }
+  }
+}
+
+const choosenPokemon = ref<PokemonDetails | null>(null)
+
+const fetched = ref(false)
 
 console.log(prop.pokemonName)
 
@@ -48,5 +81,10 @@ onBeforeMount(async () => {
 
   const response = await axios.get(url)
   choosenPokemon.value = response.data
+  fetched.value = true
 })
+
+const goToPokemonSkill = (skillName: string) => {
+  console.log(skillName)
+}
 </script>
