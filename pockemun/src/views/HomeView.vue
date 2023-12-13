72e383544576308753fcbf8a7e915d01e0bb92bf
Canthar90 <d.cieslak90@gmail.com>
2023-12-13 22:43:00 +0100

    Dividing hero part of home page to different component to simplify content

--- pockemun/src/views/HomeView.vue
+++ pockemun/src/views/HomeView.vue
@@ -1,14 +1,5 @@
 <template>
-  <div
-    class="w-full h-screen bg-center bg-cover bg-no-repeat flex items-center justify-center"
-    style="
-      background-image: url('https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/0906b988-7e73-42cc-9161-23f7d070a0e0/derph79-29509d72-4009-4bf6-863a-8877f9935b27.jpg/v1/fill/w_1154,h_692,q_70,strp/pokemon_generation_3_megas_by_mcgmark_derph79-pre.jpg?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7ImhlaWdodCI6Ijw9OTAwIiwicGF0aCI6IlwvZlwvMDkwNmI5ODgtN2U3My00MmNjLTkxNjEtMjNmN2QwNzBhMGUwXC9kZXJwaDc5LTI5NTA5ZDcyLTQwMDktNGJmNi04NjNhLTg4NzdmOTkzNWIyNy5qcGciLCJ3aWR0aCI6Ijw9MTUwMCJ9XV0sImF1ZCI6WyJ1cm46c2VydmljZTppbWFnZS5vcGVyYXRpb25zIl19.rCdwYcjK24jB0jGKTm-9sQBQtk4pbpgVox6b4kUiORg');
-    "
-  >
-    <div>
-      <h1 class="text-4xl text-center text-slate-950">Pokémon World Encyclopedia</h1>
-    </div>
-  </div>
+  <home-hero />
   <div
     class="w-full h-full bg-center bg-cover bg-fixed"
     style="
@@ -27,7 +18,7 @@
         </div>
         <div class="flex justify-center justify-items-center mt-4">
           <div class="grid grid-cols-8 p-2 justify-items-center gap-8">
-            <div
+            <button
               v-for="pokemon in displayedContent"
               :key="pokemon.name"
               role="button"
@@ -36,7 +27,7 @@
               <div class="p-2 bg-red-500 rounded-xl">
                 <h1 class="flex flex-auto text-md text-orange-100 p-2">{{ pokemon.name }}</h1>
               </div>
-            </div>
+            </button>
           </div>
         </div>
       </div>
@@ -74,6 +65,7 @@
 <script setup lang="ts">
 import axios from 'axios'
 import { onBeforeMount, ref, computed } from 'vue'
+import HomeHero from '@/components/MainAssets/HomeHero.vue'
 
 import { useRouter } from 'vue-router'
 
